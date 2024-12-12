f2ac653fa749262ead230ec7763612f110033f13
Eungyo <72914431+seoag1256@users.noreply.github.com>
2024-12-13 02:57:45 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -21,8 +21,55 @@ function preload() {
 
 function setup() {
   createCanvas(640, 640);
+
+  // 1번 음원
+  let title1 = createP("[Whiplash - aespa]");
+  title1.style("text-align", "0", "650");
+  title1.style("font-size", "18px");
+  title1.style("margin-top", "5px");
+  
+  button1 = createButton("play");
+  button1.mousePressed(playMusic1);
+  button2 = createButton("pause");
+  button2.mousePressed(pauseMusic1);
+  button1.position(160, 650);
+  button2.position(210, 650);
+  
+  // 2번 음원
+  let title2 = createP("[Supernova - aespa]");
+  title2.style("text-align", "0", "655");
+  title2.style("font-size", "18px");
+  title2.style("margin-top", "10px");
+  
+  button3 = createButton("play");
+  button3.mousePressed(playMusic);
+  button4 = createButton("pause");
+  button4.mousePressed(pauseMusic);
+  button3.position(170, 690);
+  button4.position(220, 690);
 }
 
 function draw() {
   background(220);
+}
+
+function playMusic1() {
+  if (!mySound.isPlaying()) {
+    mySound.play();
+    button.html("stop");
+  } else {
+    mySound.stop();
+    button.html("play");
+  }
+}
+
+function pauseMusic1() {
+  if (!mySound.isPlaying()) {
+    mySound.play();
+    button1.html("pause");
+  } else {
+    mySound.pause();
+    button.html("play");
+    button1.html("pause");
+  }
 }
